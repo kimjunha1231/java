dd14d704b8a7d44339ca7af48681c639c1a0411a
junha kim <tto9011@naver.com>
2023-02-10 18:40:25 +0900

    dd

--- Chap_01/HelloWorld.java
+++ Chap_01/HelloWorld.java
@@ -1,3 +1,4 @@
+package Chap_01;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("안녕하세요?");
---
+++ Chap_01/_03_Variables.java
@@ -0,0 +1,11 @@
+package Chap_01;
+
+public class _03_Variables {
+    public static void main(String[] args) {
+        String name;
+        name = "김준하";
+        System.out.println(name + "님, 배송이 시작됩니다. 15시에 방문 예정입니다.");
+        System.out.println(name + "님, 배송이 완료되었습니다.");
+    }
+
+}
