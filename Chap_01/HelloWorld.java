9532e71960fd5969dda1cbd9c4befde9d3a80a42
junha kim <tto9011@naver.com>
2023-02-10 18:32:47 +0900

    first commit

---
+++ Chap_01/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요?");
+    }
+    
+}
---
+++ Chap_01/_02_DataTypes.java
@@ -0,0 +1,14 @@
+package Chap_01;
+
+public class _02_DataTypes {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("안녕하세요");
+        System.out.println(12);
+        System.out.println(-34);
+        System.out.println(3.14);
+        System.out.println(true);
+        System.out.println(false);
+        System.out.println(123+345);
+    }
+}
