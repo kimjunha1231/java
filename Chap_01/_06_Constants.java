f694440ffcf1677ec1f33fabf69d078c9261cef1
junha kim <tto9011@naver.com>
2023-02-14 23:25:14 +0900

    dd

--- Chap_01/_06_Constants.java
+++ Chap_01/_06_Constants.java
@@ -3,8 +3,11 @@ package Chap_01;
 public class _06_Constants {
     public static void main(String[] args) {
         final String KR_COUNTRY_CODE = "+82"; // 국가 번호
-        KR_COUNTRY_CODE = "+8282";
+        // KR_COUNTRY_CODE = "+8282";
         System.out.println(KR_COUNTRY_CODE);
+
+        final double PI = 3.141592; // 원주율
+        final String DATE_OF_BIRTH = "2001-06-10"; // 생년월일
     }
     
 }
