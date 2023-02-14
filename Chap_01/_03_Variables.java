e86b793f827084e558c09a3f5157ef455934ba6e
junha kim <tto9011@naver.com>
2023-02-14 21:27:59 +0900

    ㅇㅇ

--- Chap_01/_03_Variables.java
+++ Chap_01/_03_Variables.java
@@ -2,10 +2,28 @@ package Chap_01;
 
 public class _03_Variables {
     public static void main(String[] args) {
-        String name;
-        name = "김준하";
-        System.out.println(name + "님, 배송이 시작됩니다. 15시에 방문 예정입니다.");
-        System.out.println(name + "님, 배송이 완료되었습니다.");
-    }
+        String name = "김준하";
+        int hour = 15;
 
+        System.out.println(name + "님, 배송이 시작됩니다." + hour + "시에 방문 예정입니다.");
+        System.out.println(name + "님, 배송이 완료되었습니다.");
+
+        double score = 90.5;
+        char grade = 'A';
+        name = "강백호";
+        System.out.println(name + "님의 평균 점수는" + score + "점입니다.");
+        System.out.println("학점은 " + grade + "입니다.");
+
+        boolean pass = true;
+        System.out.println("이번 시험은 합격했을까요?" + pass);
+
+        double d = 3.14123456789;
+        float f = 3.14123456789f;
+        System.out.println(d);
+        System.out.println(f);
+
+        long l = 1000000000000000L;
+        l = 1_000_000_000_000L;
+        System.out.println(l);
+    }
 }
