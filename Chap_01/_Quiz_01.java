5a1c020548f96509b95b7147cc08d5d0a3e13927
junha kim <tto9011@naver.com>
2023-02-15 00:08:29 +0900

    dd

---
+++ Chap_01/_Quiz_01.java
@@ -0,0 +1,12 @@
+package Chap_01;
+
+public class _Quiz_01 {
+    public static void main(String[] args) {
+        String bus = "상암08";
+        int time = 3;
+        float distance = 1.2F;
+        System.out.println(bus + "번 버스");
+        System.out.println("약 " + time + "분 후 도착");
+        System.out.println("남은 거리 " + distance + "km");
+    }
+}
---
+++ Chap_02/_01_Operator1.java
@@ -0,0 +1,54 @@
+package Chap_02;
+
+public class _01_Operator1 {
+    public static void main(String[] args) {
+        // 산술 연산자
+
+        // 일반 연산자
+        System.out.println(4 + 2); // 6
+        System.out.println(4 - 2); // 2
+        System.out.println(4 * 2); // 8
+        System.out.println(4 / 2); // 2 
+        System.out.println(5 / 2); // 2
+        System.out.println(2 / 4); // 0
+        System.out.println(4 % 2); // 0
+        System.out.println(5 % 2); // 1
+
+        // 우선 순위 연산
+        System.out.println(2 + 2 * 2); // 8 or 6
+        System.out.println((2 + 2) * 2); // 8
+        System.out.println(2+ (2 * 2)); // 6
+
+        // 변수를 이용한 연산
+        int a  = 20;
+        int b = 10;
+        int c;
+        
+        c = a + b;
+        System.out.println(c);//30
+        
+        c = a - b;
+        System.out.println(c); // 10
+        
+        c = a * b;
+        System.out.println(c); // 200
+        
+        c = a / b;
+        System.out.println(c); // 2
+
+        c= a % b;
+        System.out.println(c); // 0
+
+        // 증감 연산 ++, --
+        int val;
+        val = 10;
+        System.out.println(val); // 10
+        System.out.println(++val); // 11
+        System.out.println(val); // 11 
+         
+        val = 10;
+        System.out.println(val); // 10
+        System.out.println(val++); // 10
+        System.out.println(val);
+    }
+}
