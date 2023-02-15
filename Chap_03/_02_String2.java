78d0734d946fe741d48bc6bf3ce150a11b956dea
junha kim <tto9011@naver.com>
2023-02-15 17:48:36 +0900

    dd

---
+++ Chap_03/_02_String2.java
@@ -0,0 +1,10 @@
+package Chap_03;
+
+public class _02_String2 {
+    public static void main(String[] args) {
+        String s = "I like Java and Python and C.";
+
+        // 문자열 변환
+        System.out.println(s.replace("and", ","));
+    }
+}
