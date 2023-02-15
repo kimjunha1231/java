c15b065db2bcbb44a623e6a805ccaee352aa13e2
junha kim <tto9011@naver.com>
2023-02-15 16:04:17 +0900

    dd

---
+++ Chap_02/_02_Operator2.java
@@ -0,0 +1,19 @@
+package Chap_02;
+
+import javax.sound.sampled.SourceDataLine;
+
+public class _02_Operator2 {
+    // 대입 연산자
+    int num = 10;
+    num = num + 2;
+    System.out.println(num); // 12
+
+    num = num - 2;
+    System.out.println(num); // 10
+
+    num = num * 2;
+    System.out.println(num) // 20 
+    
+    num = num % 2;
+    System.out.println(num) // 0
+}
