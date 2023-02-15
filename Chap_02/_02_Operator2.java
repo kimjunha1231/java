02e4a558097ac99f187b68a8d3ec3c162ae030fe
junha kim <tto9011@naver.com>
2023-02-15 16:19:51 +0900

    dd

--- Chap_02/_02_Operator2.java
+++ Chap_02/_02_Operator2.java
@@ -1,8 +1,8 @@
 package Chap_02;
 
-import javax.sound.sampled.SourceDataLine;
 
 public class _02_Operator2 {
+    public static void main(String[] args) {
     // 대입 연산자
     int num = 10;
     num = num + 2;
@@ -12,8 +12,33 @@ public class _02_Operator2 {
     System.out.println(num); // 10
 
     num = num * 2;
-    System.out.println(num) // 20 
+    System.out.println(num); // 20 
     
     num = num % 2;
-    System.out.println(num) // 0
+    System.out.println(num); // 0
+    
+    // 복합 대입 연산자 
+    num = 10; 
+    // num = num + 2;
+    num += 2;
+    System.out.println(num); // 12
+
+    // num = num - 2;
+    num -= 2;
+    System.out.println(num); // 20
+
+    // num = num * 2;
+    num *= 2; 
+    System.out.println(num); // 20
+
+    // num = num / 2;
+    num /= 2;
+    System.out.println(num); // 10
+
+    //num = num % 2;
+    num %= 2;
+    System.out.println(num); // 0
+
+    }
+    
 }
