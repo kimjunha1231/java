d243b5d79a44cefde5b482e29ae0f84149760a87
junha kim <tto9011@naver.com>
2023-02-27 22:49:54 +0900

    ㅇㅇ

--- Chap_06/_01_Method.java
+++ Chap_06/_01_Method.java
@@ -1,7 +1,18 @@
 package Chap_06;
 
 public class _01_Method {
+    // 메소드 정의
+    public static void sayHello(){
+        System.out.println("안녕하세요? 메소드입니다.");
+    }
+
     public static void main(String[] args) {
-        
+        // 메소드 호출
+        System.out.println("메소드 호출 전");
+        sayHello();
+        sayHello();
+        sayHello();
+        System.out.println("메소드 호출 후");
+
     }
 }
