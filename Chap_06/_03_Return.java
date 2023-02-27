d16b017c7a5a0bee29f12e9496363a8da1e7d806
junha kim <tto9011@naver.com>
2023-02-27 23:04:31 +0900

    dqw

---
+++ Chap_06/_03_Return.java
@@ -0,0 +1,28 @@
+package Chap_06;
+
+public class _03_Return {
+    // 호텔 전화번호
+    public static String getPhoneNumber(){
+        String phoneNumber = "02-1234-5678";
+        return phoneNumber;
+    }
+
+    // 호텔 주소
+    public static String getAddress(){
+        return "서울시 어딘가";
+    }
+
+    // 호텔 액티비티
+    public static String getActivities() {
+        return "볼링장, 탁구장, 노래방";
+    }
+
+    public static void main(String[] args) {
+        // 반환값, Return
+        String contactNumber = getPhoneNumber();
+        System.out.println(contactNumber); 
+        String address = getAddress();
+        System.out.println("호텔 주소 : " + address); 
+        System.out.println("호텔 액티비티 : " + getActivities()); 
+    }
+}
